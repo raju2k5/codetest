5d471c19dc58a9bcf256e6d289a77bc1f8fbb27c
raju2k5 <raju2k5@gmail.com>
2024-08-06 18:32:48 +0530

    Update Handler.java

--- Main/Handler.java
+++ Main/Handler.java
@@ -1,1 +1,44 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
+@Service
+
+public class Handler {
+    private static final String SOURCE_BUCKET_NAME = "sourceBucketName";
+    private static final String SOURCE_FILE_KEY = "sourceFileKey";
+    private static final String DESTINATION_BUCKET_NAME = "destinationBucketName";
+    private static final String DESTINATION_FILE_KEY = "destinationFileKey";
+    private static final String PROCESS_FILE_NAME = "fileTobeProcessed";
+
+    @Autowired
+    public Handler() {}
+
+    public Map apply(final Map<String, String> event){
+        log.info("Received : {}", event);
+
+        event.keyset().forEach(record -> log.info(" Key = {}, Value = {}", record, event.get(record)));
+
+        String sourceBucketName = event.get(SOURCE_BUCKET_NAME);
+        String sourceFileKey = event.get(SOURCE_FILE_KEY);
+        String destinationBucketName = event.get(DESTINATION_BUCKET_NAME);
+        String destinationFileKey = event.get(DESTINATION_FILE_KEY);
+        String fileTobeProcessed = event.get(PROCESS_FILE_NAME);
+
+        String jsonSchema = "";
+
+
+        try{
+            S3Service.convertCsvToParquetAndUpload(sourceBucketName, sourceFileKey, jsonSchema);
+        } catch (Exception e){
+            throw new RuntimeException("Exception occured during file convertion..")
+        }
+
+        final Map<String, Object> response = new HashMap<>();
+
+        return response;
+    }
+}
